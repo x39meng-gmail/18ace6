3c771273c935658576859fbc9da8289eb4be1706
LAPTOP-C3P0RVJL <>
2022-02-11 13:29:28 -0500

    Bug fix: Sending messages

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -12,12 +12,13 @@ export const addMessageToStore = (state, payload) => {
   }
 
   return state.map((convo) => {
-    if (convo.id === message.conversationId) {
-      convo.messages.push(message);
-      convo.latestMessageText = message.text;
-      return convo;
+    const newConvo = {...convo}
+    if (newConvo.id === message.conversationId) {
+      newConvo.messages.push(message);
+      newConvo.latestMessageText = message.text;
+      return newConvo;
     } else {
-      return convo;
+      return newConvo;
     }
   });
 };
